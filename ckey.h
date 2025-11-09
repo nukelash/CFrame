07682b41cdb2d82d240c3aac2f08ef1d9ffc99e1
nukelash <lnash016@gmail.com>
2025-11-09 13:57:08 -0500

    updates

--- ckey.h
+++ ckey.h
@@ -29,6 +29,7 @@ typedef struct {
     //read-only
     int __KeyframeIndex;
     bool __Held;
+    Keyframe __Modification;
 } TransformContext;
 
 // typedef struct {
@@ -87,30 +88,38 @@ float QuadraticEaseInOut(float p)
 	}
 }
 
+float BounceEaseOut(float p)
+{
+	if(p < 4/11.0)
+	{
+		return (121 * p * p)/16.0;
+	}
+	else if(p < 8/11.0)
+	{
+		return (363/40.0 * p * p) - (99/10.0 * p) + 17/5.0;
+	}
+	else if(p < 9/10.0)
+	{
+		return (4356/361.0 * p * p) - (35442/1805.0 * p) + 16061/1805.0;
+	}
+	else
+	{
+		return (54/5.0 * p * p) - (513/25.0 * p) + 268/25.0;
+	}
+}
+
 Vector2 Transform_Vector2(TransformContext* ctx, Vector2 vector) {
+
+    //TODO: Need to do some sort of system that recursively applies all previous modifications so that easing in the second keyframe starts from the end of the first keyframe, not the initial position.
+    //Thinking basically a modification Keyframe object that gets applied to the input vector every frame. Each frame that modification Keyframe gets cumulatively adjusted by the timeline of keyframes.
+    // For scrubbing backwards, the modifications just get undone off the modification Keyframe
+
+
     // calculate which keyframe
     Vector2 return_vector;
-    // int keyframe_idx = 0;
-    // bool held;
-    // int index_count = 0;
-    // for (int i = 0; i < ctx->NumKeyframes; i++) {
-    //     index_count += ctx->Keyframes[i].EasingFrames;
-    //     if (ctx->Index < index_count) {
-    //         keyframe_idx = i;
-    //         held = false;
-    //         break;
-    //     }
-    //     index_count += ctx->Keyframes[i].HeldFrames;
-    //     if (ctx->Index < index_count) {
-    //         keyframe_idx = i;
-    //         held = true;
-    //         break;
-    //     }
-    // }
-    //calculate how far into keyframe
-    //calculate true transform values based on index
+
     if(ctx->__Held) {
-        printf("holding\n");
+        //printf("holding\n");
         return_vector.x = vector.x + ctx->Keyframes[ctx->__KeyframeIndex].AOffset;
         return_vector.x *= ctx->Keyframes[ctx->__KeyframeIndex].AScale;
 
@@ -119,20 +128,20 @@ Vector2 Transform_Vector2(TransformContext* ctx, Vector2 vector) {
     }
     else {
         int SubKeyframeIdx, Difference = 0;
-        for (int i = 1; i < ctx->__KeyframeIndex; i++) {
+        for (int i = 1; i <= ctx->__KeyframeIndex; i++) {
             Difference += ctx->Keyframes[i-1].EasingFrames;
             Difference += ctx->Keyframes[i-1].HeldFrames;
         }
         SubKeyframeIdx = ctx->Index - Difference;
         float ModFactor = QuadraticEaseInOut(((float)SubKeyframeIdx / (float)ctx->Keyframes[ctx->__KeyframeIndex].EasingFrames));
 
-        //printf("%f %f\n", ((float)SubKeyframeIdx / (float)ctx->Keyframes[ctx->__KeyframeIndex].EasingFrames), ModFactor);
+        //printf("Difference: %d in: %f modfactor: %f\n", Difference, ((float)SubKeyframeIdx / (float)ctx->Keyframes[ctx->__KeyframeIndex].EasingFrames), ModFactor);
 
         return_vector.x = vector.x + (ModFactor * ctx->Keyframes[ctx->__KeyframeIndex].AOffset);
-        return_vector.x *= (1 + (ModFactor * (1 - ctx->Keyframes[ctx->__KeyframeIndex].AScale)) );
+        return_vector.x *= (1 + (ModFactor * (ctx->Keyframes[ctx->__KeyframeIndex].AScale - 1)) );
 
         return_vector.y = vector.y + (ModFactor * ctx->Keyframes[ctx->__KeyframeIndex].BOffset);
-        return_vector.y *= (1 + (ModFactor * (1 - ctx->Keyframes[ctx->__KeyframeIndex].BScale)) );
+        return_vector.y *= (1 + (ModFactor * (ctx->Keyframes[ctx->__KeyframeIndex].BScale - 1)) );
 
     }
     
@@ -145,7 +154,7 @@ Vector2 Transform_Vector2(TransformContext* ctx, Vector2 vector) {
         }
         ctx->Index++;
         ctx->Index = ctx->Index % MaxIndex;
-        printf("%d\n", ctx->Index);
+        //printf("%d\n", ctx->Index);
 
         //set __KeyframeIndex
         int accumulation = 0;
--- example.c
+++ example.c
@@ -10,17 +10,20 @@ int main() {
     InitWindow(680, 400, "Example");
     SetTargetFPS(60);
 
-    Keyframe frames[1] = {
+    Keyframe frames[2] = {
         // InitKeyframe((Keyframe){.AOffset = 3, .BScale = 1.2f, .EasingFrames=60, .HeldFrames=60}),
-        // InitKeyframe((Keyframe){.AScale = 0.8, .EasingFrames=60, .HeldFrames=60}),
-        KEYFRAME({.BOffset = 70, .EasingFrames=20, .HeldFrames=60})
+        InitKeyframe((Keyframe){.AScale = 2, .EasingFrames=20, .HeldFrames=60}),
+        KEYFRAME({.AScale = 1.5, .BOffset = 70, .EasingFrames=20, .HeldFrames=60})
         };
     
     for(int i = 0; i < 3; i++) {
         printf("%f, %f, %f, %f\n", frames[i].AOffset, frames[i].BOffset, frames[i].AScale, frames[i].BScale);
     }
 
-    TransformContext ctx = {.Keyframes = frames, .NumKeyframes=1};
+    TransformContext ctx = {.Keyframes = frames, .NumKeyframes=2};
+
+    Keyframe dim_frames[1] = {KEYFRAME({.BScale=3, .EasingFrames=30, .HeldFrames=60})};
+    TransformContext dim_ctx = {.Keyframes = dim_frames, .NumKeyframes=1};
 
     Vector2 position = {30,30};
     Vector2 dimensions = {100,100};
@@ -30,6 +33,9 @@ int main() {
         if(IsKeyPressed(KEY_A)) {
             ctx.Playing = !ctx.Playing;
         }
+        if(IsKeyPressed(KEY_S)) {
+            dim_ctx.Playing = !dim_ctx.Playing;
+        }
         //printf("Playing: %d\n", ctx.Playing);
 
         BeginDrawing();
@@ -37,7 +43,8 @@ int main() {
             ClearBackground(RED);
 
             Vector2 new_position = Transform_Vector2(&ctx, position);
-            DrawRectangle(new_position.x, new_position.y, dimensions.x, dimensions.y, BLACK);
+            Vector2 new_dims = Transform_Vector2(&dim_ctx,dimensions);
+            DrawRectangle(new_position.x, new_position.y, new_dims.x, new_dims.y, BLACK);
         
         EndDrawing();
     }
