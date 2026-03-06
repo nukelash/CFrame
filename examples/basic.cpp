7b1c6e8163735073d924954ade98003eace68029
nukelash <lnash016@gmail.com>
2026-03-06 13:52:12 -0500

    example cleanup

--- examples/basic.cpp
+++ examples/basic.cpp
@@ -1,37 +1,7 @@
 #include "raylib.h"
 #include "../cframe.hpp"
 
-
-
-/*
-MAYBE the way forward is just a single T struct, and an enum that describes the transformation i.e. CF_TRANSFORM_TO, CF_TRANSFORM_ADD etc. Then the T types themselves don't need to be optional
-BUT then I'd still need each T member to be optional... maybe that's where the boost pfr library comes in.
-*/
-
-/*
-
-Rectangle rec = {1, 2, 3, 4};
-
-CF::Animated<Rectangle> anim_rec(rec, keyframes)
-
-if (mouse_clicked) {
-    anim_rec.Play();
-    anim_rec.playing = true; // I think I like this better -- more control to the user
-}
-
-DrawRectanlgeRec(anim_rec.Get(), BLUE); // Next(), Get(), Step(), Process(), etc..
-
-vs
-DrawRectangleRec(anim_rec.Process(rec), BLUE); // with this option, rec, doesn't need to get defined before anim_rec, it can do it to any rectangle. Is this truly helpful?
-
-I think the first option, where an Animated<> object is instantiated by the OG object and its keyframes, is pretty straightforward and elegant
-*/
-
-
-
 int main() {
-
-    sin(30);
 
     InitWindow(680, 400, "Basic Example");
     SetTargetFPS(60);
@@ -47,15 +17,14 @@ int main() {
         }
     };
 
-    Rectangle rec = {100, 100, 100, 100};
-    CF::Animation<Rectangle> moving_rectangle(rec, keyframes);
+    CF::Animation<Rectangle> rec({100, 100, 100, 100}, keyframes);
 
     while(!WindowShouldClose()) {
 
         BeginDrawing();
 
             ClearBackground(LIGHTGRAY);
-            DrawRectangleRec(moving_rectangle.Next(), BLUE);
+            DrawRectangleRec(rec.Next(), BLUE);
    
         EndDrawing();
     }
--- examples/colors.cpp
+++ examples/colors.cpp
@@ -3,10 +3,6 @@
 
 #include <vector>
 
-// CF_DEFINE_4(Rectangle, float, x, y, width, height);
-// CF_DEFINE_4(Color, unsigned char, r, g, b, a);
-// CF_DEFINE_2(Vector2, float, x, y);
-
 struct myfloat {
     float f;
 };
@@ -15,24 +11,6 @@ int main() {
 
     InitWindow(680, 400, "Color Animation");
     SetTargetFPS(60);
-
-
-    // CF_RectangleKeyframe rec_keyframes[3] = {
-    //     CF_InitRectangleKeyframe((CF_RectangleKeyframe){
-    //         .Add.x=200, 
-    //         .EasingFrames=60, 
-    //         .HeldFrames=10}), 
-    //     CF_InitRectangleKeyframe((CF_RectangleKeyframe){
-    //         .Add.x=-100, 
-    //         .Add.y=-200, 
-    //         .EasingFrames=60, 
-    //         .HeldFrames=10}),
-    //     CF_InitRectangleKeyframe((CF_RectangleKeyframe){
-    //         .Add.x=-100, 
-    //         .Add.y=200, 
-    //         .EasingFrames=60, 
-    //         .HeldFrames=10})
-    // };
 
     std::vector<CF::Keyframe<Rectangle>> rectangle_keyframes {
         {
@@ -55,24 +33,6 @@ int main() {
         }
     };
 
-    // CF_ColorKeyframe color_keyframes[3] = {
-    //     CF_InitColorKeyframe((CF_ColorKeyframe){
-    //         .Add.r=-255,
-    //         .Add.g=255, 
-    //         .EasingFrames=60,
-    //         .HeldFrames=10}),
-    //     CF_InitColorKeyframe((CF_ColorKeyframe){
-    //         .Add.g=-255,
-    //         .Add.b=255, 
-    //         .EasingFrames=60,
-    //         .HeldFrames=10}),
-    //     CF_InitColorKeyframe((CF_ColorKeyframe){
-    //         .Add.r=255,
-    //         .Add.b=-255,
-    //         .EasingFrames=60,
-    //         .HeldFrames=10})
-    // };
-
     std::vector<CF::Keyframe<Color>> color_keyframes = {
         {
             .transform = {.r = 15, .g = 255},
@@ -84,13 +44,6 @@ int main() {
         }
     };
 
-    // CF_Vector2Keyframe v2_keyframes[1] = {
-    //     CF_InitVector2Keyframe((CF_Vector2Keyframe){
-    //         .Add.x=1,
-    //         .EasingFrames=120
-    //     })
-    // };
-
     std::vector<CF::Keyframe<float>> roundness_keyframes = {
         {
             .transform = 1.0f,
@@ -99,48 +52,17 @@ int main() {
         }
     };
 
-    // CF_Vector2Context v2_ctx = {
-    //     .Keyframes = v2_keyframes,
-    //     .NumKeyframes = 1,
-    //     .Mode=CF_PLAYMODE_BOOMERANG_LOOP,
-    //     .Playing=true
-    // };
-
     CF::Animation<float> roundness = {0.0f, roundness_keyframes, CF::PlayMode::BOOMERANG_LOOP, CF::EasingFunction::QUADRATIC_IN_OUT};
-
-    // CF_RectangleContext ctx_rec = {
-    //     .Keyframes=rec_keyframes,
-    //     .NumKeyframes=3,
-    //     .Mode=CF_PLAYMODE_LOOP,
-    //     .Playing=true,
-    //     .EasingFunction=CF_EASING_QUADRATIC_IN_OUT
-    // };
 
     CF::Animation<Rectangle> rectangle = {{200, 250, 60, 60}, rectangle_keyframes, CF::PlayMode::LOOP, CF::EasingFunction::QUADRATIC_IN_OUT};
 
-    // CF_ColorContext ctx_color = {
-    //     .Keyframes=color_keyframes,
-    //     .NumKeyframes=3,
-    //     .Mode=CF_PLAYMODE_BOOMERANG_LOOP,
-    //     .Playing=true,
-    //     .EasingFunction=CF_EASING_CUBIC_IN_OUT
-    // };
-
     CF::Animation<Color> color = {{255, 0, 0, 255}, color_keyframes, CF::PlayMode::BOOMERANG_LOOP, CF::EasingFunction::CUBIC_IN_OUT};
-
-    // Rectangle rec = {200, 250, 60, 60};
-    // Color c = {255, 0, 0, 255};
-    // Vector2 roundness = {0, 0};
 
     while(!WindowShouldClose()) {
 
         BeginDrawing();
 
             ClearBackground(LIGHTGRAY);
-
-            // float r = CF_Vector2Process(&v2_ctx, roundness).x;
-
-            // DrawRectangleRounded(CF_RectangleProcess(&ctx_rec, rec), r, 8, CF_ColorProcess(&ctx_color, c));
 
             DrawRectangleRounded(rectangle.Next(), roundness.Next(), 8, color.Next());
    
